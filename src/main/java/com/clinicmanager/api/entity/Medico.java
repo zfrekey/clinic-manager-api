7c555f50336944f69a0f52e1c5badfea85b5afb3
zfrekey <filype.abreu@gmail.com>
2026-02-14 18:26:10 -0300

    fix: Correções na entidade de endereço

--- src/main/java/com/clinicmanager/api/entity/Medico.java
+++ src/main/java/com/clinicmanager/api/entity/Medico.java
@@ -1,15 +1,22 @@
 package com.clinicmanager.api.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Table;
+import com.clinicmanager.api.dto.enums.Especialidade;
+import com.clinicmanager.api.entity.Endereco;
+import jakarta.persistence.*;
 
 @Table(name= "medicos")
 @Entity(name = "Medico")
 public class Medico {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
     private String crm;
+
+    @Enumerated(EnumType.STRING)
+    private Especialidade especialidade;
+
+    @Embedded
+    private Endereco Endereco;
 }
